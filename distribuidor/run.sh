f4ed7faf6bda7c2419e8a76c98f1b6cd269d2af6
jariza <jariza@ieee.org>
2024-12-08 19:31:19 +0100

    Bugfixing

--- distribuidor/run.sh
+++ distribuidor/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+kill `pgrep -f 'distribuidor' -o`
 source venv/bin/activate
 gunicorn --access-logfile - -b 0.0.0.0:80 main:app --daemon
--- generador/run.sh
+++ generador/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+kill `pgrep -f 'generador' -o`
 source venv/bin/activate
 gunicorn --access-logfile - -b 0.0.0.0:5001 main:app --daemon
